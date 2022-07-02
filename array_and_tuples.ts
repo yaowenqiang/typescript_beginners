7e9f3ce2e3df5bdf40f664774c7e5bc971aa6fb6
姚文强 <yaowenqiang111@163.com>
2022-07-02 21:55:59 +0800

    add arrays and tuple datatype demo

---
+++ array_and_tuples.ts
@@ -0,0 +1,17 @@
+const  teamss: string[] = [
+    'jack'
+]
+
+const nums : Array<number> = [1,2,3]
+
+
+const  teams2: (string|number)[] = [
+    'jack',
+    123,
+]
+
+// tuples
+
+const my_tuples :[number, string] = [1010, "good year"]
+// const my_tuples2 :[number, string] = ["good year", 1010 ]
+
